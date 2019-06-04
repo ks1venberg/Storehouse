ba2ff70b7a25522ee2ad3c4a550b93116a02e27e
ks1venberg <venberg.dev@gmail.com>
2019-06-04 19:51:15 +0300

    filled migrations, added join_table

--- db/migrate/20190604142531_create_shops.rb
+++ db/migrate/20190604142531_create_shops.rb
@@ -1,8 +1,11 @@
 class CreateShops < ActiveRecord::Migration[5.2]
   def change
-    create_table :shops do |t|
-
-      t.timestamps
+  	create_table :shops do |t|
+			t.string :shop_name,		null: false
+			t.string :address,			null: false
+			t.string :subway,				null: true
+			t.string :city,					null: false
+			t.timestamps
     end
   end
 end
--- db/migrate/20190604143500_create_products.rb
+++ db/migrate/20190604143500_create_products.rb
@@ -1,8 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
-      t.timestamps
+		  t.string 	:artnumber, null: false
+		  t.string 	:title,			null: false
+			t.integer :weight,		null: false
+			t.string 	:size,			null: false
+			t.string 	:color,			null: false
+			t.decimal :price,			null: false
+			t.integer :estcount,	null: false
+		  t.timestamps
     end
   end
 end
